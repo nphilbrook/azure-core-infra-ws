40f25e82c8a07d04c3600bc3b282a296f3e35092
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-10-27 15:27:25 -0400

    Add environment to environment_info :eyeroll:

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,7 @@ output "resource_group_location" {
 
 output "environment_info" {
   value = {
+    environment         = var.environment
     location            = var.location
     resource_group_name = azurerm_resource_group.rg.name
     zone_name           = azurerm_dns_zone.zone.name
